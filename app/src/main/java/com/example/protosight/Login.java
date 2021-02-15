88af435666c44528c563393a14abb227e13fae46
CrazyKs <kodks.hu@gmail.com>
2021-02-15 15:24:35 -0500

    Login backend done (#4)
    * backend for login
    
    * backend for login done

--- app/src/main/java/com/example/protosight/Login.java
+++ app/src/main/java/com/example/protosight/Login.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.protosight.models.Creator;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -27,6 +30,12 @@ public class Login extends AppCompatActivity {
         EditText password = (EditText) findViewById(R.id.password);
 
         mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            Log.d(TAG, "Curruent Logged in User: " + mAuth.getCurrentUser().getEmail());
+            Intent intent = new Intent(this, CreatorLandingPage.class);
+            startActivity(intent);
+        }
     }
 
     public void signInOnClick(View view){
@@ -41,7 +50,8 @@ public class Login extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "signInWithEmail:success");
-                                // Jump to profile page
+                                Intent intent = new Intent(Login.this, CreatorLandingPage.class);
+                                startActivity(intent);
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w(TAG, "signInWithEmail:failure", task.getException());
--- app/src/main/java/com/example/protosight/SignUp.java
+++ app/src/main/java/com/example/protosight/SignUp.java
@@ -1,24 +1,120 @@
 package com.example.protosight;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+
 import android.widget.Toast;
 
+import com.example.protosight.models.Creator;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+
+
 public class SignUp extends AppCompatActivity {
+
+    private EditText usernameText, passwordText, emailText, confirmPasswordText;
+    private FirebaseAuth mAuth;
+    private String TAG = "SignUpPage";
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        usernameText = (EditText) findViewById(R.id.username);
+        passwordText = (EditText) findViewById(R.id.password);
+        emailText = (EditText) findViewById(R.id.email);
+        confirmPasswordText = (EditText) findViewById(R.id.confirm_password);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void registerOnClick(View view){
+        String username, password, confirmPassword, email;
+        username = usernameText.getText().toString();
+        password = passwordText.getText().toString();
+        confirmPassword = confirmPasswordText.getText().toString();
+        email = emailText.getText().toString();
+
+        if (validate_input(username, password, confirmPassword, email)){
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                // Save to db and bitmap uir to storage
+                                Creator creator = new Creator(
+                                        username,
+                                        email,
+                                        mAuth.getCurrentUser().getUid()
+                                );
+
+                                // Direct to login page
+                                saveCreatorToFirebase(creator);
+                                Log.d(TAG, "createUserWithEmail:success");
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SignUp.this, task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+
+    private void saveCreatorToFirebase(Creator user){
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Creating user...");
+        progressDialog.show();
+        // save user to db
+        db.collection("creators").document(user.getUid()).set(user.toMap())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "createUserToFireStore:success");
+                        progressDialog.dismiss();
+                        Intent intent = new Intent(SignUp.this, Login.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUp.this, "Fail to upload image profile. Will use the default one",
+                                Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "createUserToFireStore:failed");
+                    }
+                });
 
 
     }
+
+
 
 
 
