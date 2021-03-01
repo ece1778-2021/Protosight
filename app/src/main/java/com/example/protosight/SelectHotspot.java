59bfb91479ec04ee903c7d9845785472039cbefa
CrazyKsv <kodks.hu@gmail.com>
2021-03-01 01:19:02 -0500

    be able to do slide up panel

--- app/src/main/java/com/example/protosight/SelectHotspot.java
+++ app/src/main/java/com/example/protosight/SelectHotspot.java
@@ -1,8 +1,13 @@
 package com.example.protosight;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.res.Resources;
@@ -13,11 +18,20 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.protosight.adapters.CreateProjectImageAdapter;
+import com.example.protosight.adapters.HotspotLinkScreenAdapter;
 import com.example.protosight.models.HotSpot;
 import com.example.protosight.views.DragRectView;
 
@@ -28,6 +42,16 @@ import java.util.ArrayList;
 public class SelectHotspot extends AppCompatActivity {
     private String TAG = "SelectHotspot";
     private DragRectView view;
+    private Toolbar toolbar;
+
+    private int x,y,w,h;
+    private ImageView iv;
+    private Bitmap originImage;
+    private Bitmap newImage;
+    private ArrayList<String> images;
+    private String projectName;
+    private String current;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,56 +61,36 @@ public class SelectHotspot extends AppCompatActivity {
         Intent intent = getIntent();
 
 
-        ArrayList<String> images = intent.getStringArrayListExtra("images");
+        images = intent.getStringArrayListExtra("images");
 
 
-        String projectName = intent.getStringExtra("projectName");
-        String current = intent.getExtras().getString("selectedImage");
+        projectName = intent.getStringExtra("projectName");
+        current = intent.getExtras().getString("selectedImage");
 
-        ImageView iv = findViewById(R.id.add_hotspot_image);
-        Bitmap bitmap = BitmapFactory.decodeFile(current);
-
-
+        iv = findViewById(R.id.add_hotspot_image);
+        originImage = BitmapFactory.decodeFile(current);
 
 
-        iv.setImageBitmap(bitmap);
+//        ListView listView = findViewById(R.id.select_link_image);
+//        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{ "stest", "42"}));
 
-        BitmapDrawable drawable2 = (BitmapDrawable) iv.getDrawable();
 
-        view = (DragRectView) findViewById(R.id.dragRect);
+        Log.d(TAG, "hotspot.toString()");
+        iv.setImageBitmap(originImage);
+
+        view = findViewById(R.id.dragRect);
+
         if (view != null) {
             view.setOnUpCallback(new DragRectView.OnUpCallback() {
                 @Override
                 public void onRectFinished(final Rect rect) {
 
-                    int x = (int) (rect.left) ;
-                    int y = (int) (rect.top) ;
-                    int w = (int) (rect.width());
-                    int h = (int) (rect.height());
+                    x = (int) (rect.left) ;
+                    y = (int) (rect.top) ;
+                    w = (int) (rect.width());
+                    h = (int) (rect.height());
 
-
-                    HotSpot hotspot = new HotSpot(x,y,w,h, current);
-                    Log.d(TAG, hotspot.toString());
-
-
-
-                    // TODO: Add hotspot to image and then go to link screen
-                    Toast.makeText(SelectHotspot.this, "Rect is (" + rect.left + ", " + rect.top + ", " + rect.right + ", " + rect.bottom + ")",
-                            Toast.LENGTH_LONG).show();
                     view.setVisibility(View.INVISIBLE);
-
-
-
-                    Intent i = new Intent(SelectHotspot.this, HotspotsLinkScreen.class);
-                    Bundle b = new Bundle();
-
-                    b.putStringArrayList("images", images);
-
-                    i.putExtras(b);
-                    i.putExtra("selectedImage", current);
-                    i.putExtra("projectName", projectName);
-                    i.putExtra("hotspot", hotspot);
-
 
 
                     int imagex = (int) (rect.left) -(iv.getWidth()-iv.getDrawable().getBounds().right)/2;
@@ -96,21 +100,72 @@ public class SelectHotspot extends AppCompatActivity {
                     mRectPaint.setColor(getResources().getColor(android.R.color.holo_red_dark));
                     mRectPaint.setStyle(Paint.Style.STROKE);
                     mRectPaint.setStrokeWidth(5);
-                    Bitmap theBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                    Bitmap theBitmap = originImage.copy(Bitmap.Config.ARGB_8888, true);
                     Canvas canvas = new Canvas(theBitmap);
                     canvas.drawRect(imagex,imagey,w+imagex,imagey+h, mRectPaint);
-                    Bitmap bb = theBitmap.copy(Bitmap.Config.ARGB_8888, false);
-                    iv.setImageBitmap(bb);
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    bb.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                    byte[] byteArray = baos.toByteArray();
-                    i.putExtra("bitmapp", byteArray);
+                    newImage = theBitmap.copy(Bitmap.Config.ARGB_8888, false);
+                    iv.setImageBitmap(newImage);
 
-                    startActivity(i);
+                    toolbar = findViewById(R.id.toolbar);
+
+                    setSupportActionBar(toolbar);
+
+
 
                 }
             });
         }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.reset_confirm_link, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.reset_hotspot){
+            Log.d(TAG, "Reset.....");
+            iv.setImageBitmap(originImage);
+            view.setVisibility(View.VISIBLE);
+            view.clearFocus();
+        } else if (id == R.id.confirm_hotspot){
+            Log.d(TAG, "Confirm.....");
+            Intent i = new Intent(SelectHotspot.this, HotspotsLinkScreen.class);
+            Bundle b = new Bundle();
+            HotSpot hotspot = new HotSpot(x,y,w,h, current);
+            Log.d(TAG, hotspot.toString());
+
+            b.putStringArrayList("images", images);
+
+            i.putExtras(b);
+            i.putExtra("selectedImage", current);
+            i.putExtra("projectName", projectName);
+            i.putExtra("hotspot", hotspot);
+
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            newImage.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] byteArray = baos.toByteArray();
+            i.putExtra("bitmapp", byteArray);
+
+//            startActivity(i);
+            
+            RecyclerView rv = findViewById(R.id.select_link_image);
+
+            RecyclerView.LayoutManager layoutManager = new GridLayoutManager(SelectHotspot.this, 2);
+            rv.setLayoutManager(layoutManager);
+            CreateProjectImageAdapter ia = new CreateProjectImageAdapter(images, SelectHotspot.this, projectName);
+            rv.setAdapter(ia);
+
+
+
+        }
+        return true;
     }
 
 }
