2ab2173b33452c77030d662ebdc5c2fbd31dc2f5
CrazyKs <kodks.hu@gmail.com>
2021-03-13 23:24:18 -0500

    landing page for participant done

--- app/src/main/java/com/example/protosight/ParticipantLandingPage.java
+++ app/src/main/java/com/example/protosight/ParticipantLandingPage.java
@@ -1,14 +1,55 @@
 package com.example.protosight;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.protosight.adapters.ListAllProjectAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class ParticipantLandingPage extends AppCompatActivity {
-
+    private FirebaseFirestore db;
+    private String TAG = "ParticipantLandingPage";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participant_landing_page);
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public void testCodeOnSubmit(View view){
+        EditText editText = findViewById(R.id.test_code);
+        String code = editText.getText().toString();
+        db.collection("tests").document(code).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Toast.makeText(ParticipantLandingPage.this, "No such test code", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
     }
 }
